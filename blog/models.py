159b1bf064b5fe44703f9b7d6ba959e764fb710f
olowu abayomi <aayobam@gmail.com>
2022-06-18 01:32:54 +0100

    django model

---
+++ blog/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
